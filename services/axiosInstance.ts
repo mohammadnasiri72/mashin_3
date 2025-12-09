45a02c2f76defe1d2da1b43bd12a284a4a1ec39b
mohammad nasiri <nasiri.mohammadreza92@yahoo.com>
2025-12-09 12:11:58 +0330

    test

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -1,40 +1,10 @@
-// // axiosInstance.ts
-// import axios from "axios";
-
-// // ایجاد instance ساده بدون interceptor
-// const axiosInstance = axios.create({
-//   baseURL: "https://api3.aitest2.ir/",
-//   timeout: 10000,
-// });
-
-// export default axiosInstance;
-
-
-
 // axiosInstance.ts
 import axios from "axios";
 
-// بررسی محیط (سرور یا کلاینت)
-const isServer = typeof window === 'undefined';
-
-// تعیین baseURL بر اساس محیط
-const getBaseURL = () => {
-  if (isServer) {
-    // در سرور: مستقیماً به API متصل شو
-    return "https://api3.aitest2.ir/";
-  } else {
-    // در کلاینت: از proxy استفاده کن
-    return "/api-proxy/"; // توجه: اسلش انتها مهم است
-  }
-};
-
-// ایجاد instance
+// ایجاد instance ساده بدون interceptor
 const axiosInstance = axios.create({
-  baseURL: getBaseURL(),
-  timeout: 15000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://api3.aitest2.ir/",
+  timeout: 10000,
 });
 
 export default axiosInstance;
