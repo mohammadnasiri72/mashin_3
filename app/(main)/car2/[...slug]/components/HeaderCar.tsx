e3945ed1d8d0706400bbe059ecdfd3ba2d408dac
mohammad nasiri <nasiri.mohammadreza92@yahoo.com>
2026-08-03 19:30:02 +0330

    edit

--- app/(main)/car2/[...slug]/components/HeaderCar.tsx
+++ app/(main)/car2/[...slug]/components/HeaderCar.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 function HeaderCar({detailsCar}:{detailsCar:ItemsId}) {
   return (
     <>
-    <div  style={{
+    <div style={{
           backgroundImage:
-            `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url('/images/1.jpeg')`,
-        }} className='h-100 bg-cover bg-no-repeat bg-center'>
+            `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url('/images/1.webp')`,
+        }} className='min-h-100 bg-cover bg-no-repeat bg-center'>
          {/* Breadcrumb */}
-          <nav className="mt-6">
+          <nav className="">
             <ol className="flex items-center flex-wrap gap-1 text-sm">
               <li>
                 <Link
